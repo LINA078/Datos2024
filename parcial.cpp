8033bf47bbbd75e1f7e3f556131f4f41a93d97ba
lina <linaalomia7@gmail.com>
2024-10-05 07:45:39 -0500

    primer parcial

---
+++ parcial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+struct nodo{
+
+int documento;
+string nombre;
+string destino;
+    struct nodo*sig;
+
+};
+struct nodo*cab,*aux;
+
+
+int registrar(){
+    aux=((struct nodo*)malloc(sizeof(struct nodo)));
+
+    cout <<"CUAL ES EL NUMERO DE CEDULA DEL PASAJERO">>endl;
+    cin 
+};
+
+
