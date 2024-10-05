c9f433841841fc0329b0dc21fc43bbd9c862db79
lina <linaalomia7@gmail.com>
2024-10-05 08:53:23 -0500

    mostrar

--- parcial.cpp
+++ parcial.cpp
@@ -5,17 +5,52 @@ struct nodo{
 int documento;
 string nombre;
 string destino;
-    struct nodo*sig;
+ nodo*sig;
 
 };
-struct nodo*cab,*aux;
+struct nodo *cab = NULL, *aux=NULL, *fin=NULL;
 
 
 int registrar(){
     aux=((struct nodo*)malloc(sizeof(struct nodo)));
 
-    cout <<"CUAL ES EL NUMERO DE CEDULA DEL PASAJERO">>endl;
-    cin 
-};
+    cout <<"CUAL ES EL NUMERO DE DOCUMENTO DEL PASAJERO"<<endl;
+    cin >>aux->documento;
+    cout <<"INGRESE EL NOMBRE DEL PASAJERO"<<endl;
+    cin >>aux->nombre;
+    cout <<"CUAL ES EL DESTINO DEL PASAJERO"<<endl;
+    cin >>aux->destino;
+    aux->sig=NULL;
+
+    if(!cab){
+        cab = fin = aux;
+        
+
+        }else{
+            fin->sig=aux;
+            fin=aux;
+        }
+        aux=NULL;     
+
+       free(aux);
+       {
+
+       }
+       int mostrar();
+       {
+        for(aux=cab;
+        aux!=NULL;
+        aux=aux->sig);
+        {
+            cout <<"nodo" <<aux->documento<<endl;
+            cout<<"nodo"<<aux->nombre<<endl;
+            cout<<"nodo"<<aux->destino<<endl;
+        }
+
+
+        }
+               }
+ 
+
 
 
